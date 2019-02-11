8baa5616e32f39d1aa6d6b3789dc54aad0049d42
RCHO <RCHO@LT6624s-MacBook-Pro.local>
2019-02-11 15:35:58 -0800

    Added XCTest as quick example.

--- RH-ShowMyExternalIPTests/RH_ShowMyExternalIPTests.swift
+++ RH-ShowMyExternalIPTests/RH_ShowMyExternalIPTests.swift
@@ -13,6 +13,7 @@ class RH_ShowMyExternalIPTests: XCTestCase {
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
     }
     
     override func tearDown() {
@@ -25,6 +26,18 @@ class RH_ShowMyExternalIPTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
+    func testHelloWorld_AssertFailTestExample() {
+        let expectVal = 1
+        let actualVal = 2
+        XCTAssertEqual(expectVal, actualVal, "This is just demo assert fail a test!")
+    }
+    
+    func testHelloWorld_AssertPassTestExample() {
+        let expectVal = 1
+        let actualVal = 1
+        XCTAssertEqual(expectVal, actualVal, "This is just demo assert pass a test!")
+    }
+
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
